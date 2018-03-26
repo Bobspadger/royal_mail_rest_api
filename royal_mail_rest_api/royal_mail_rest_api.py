885e23f54f00460b56db23072aea9ee30416605f
Alex Hellier <alex@absolutemusic.co.uk>
2018-03-26 14:58:11 +0100

    add in history

--- royal_mail_rest_api/royal_mail_rest_api.py
+++ royal_mail_rest_api/royal_mail_rest_api.py
@@ -30,6 +30,9 @@ class TrackingApi(RoyalMailBaseClass):
     Start class for royal mail shipping api
     """
     summary_url = 'mailPieces/{}/summary'
+    pod_url = 'mailPieces/{}/proofOfDelivery'
+    history_url = 'mailPieces/{}/history'
+
 
     def __init__(self, client_id, client_secret):
         """
@@ -51,6 +54,7 @@ class TrackingApi(RoyalMailBaseClass):
                        'X-IBM-Client-Secret': self.client_secret,
                        'accept': 'application/json'}
 
+
     def summary(self, tracking_number):
         """
         takes 13 digit tracking number and requests summary data
@@ -63,11 +67,40 @@ class TrackingApi(RoyalMailBaseClass):
         return result.json()
 
 
+    def proof_of_delivery(self, tracking_number):
+        """
+        recover proof of delivery
+        :param tracking_number:
+        :return:
+        """
+        pod_url = self.summary_url.format(tracking_number)
+        result = requests.get('{}/{}'.format(self.url, pod_url), headers=self.header)
+        self._test_error(result)
+        return result.json()
 
 
-
-
+    def history(self, tracking_number):
+        """
+        Return history for a tracked item
+        :param tracking_number:
+        :return:
+        """
+        history_url = self.history_url.format(tracking_number)
+        result = requests.get('{}/{}'.format(self.url, history_url), headers=self.header)
+        self._test_error(result)
+        return result.json()
 
 if __name__ == '__main__':
+    CLIENT_ID = ''
+    CLIENT_SECRET = ''
+    TRACKING_NUMBER = ''
     tracking_api = TrackingApi(CLIENT_ID, CLIENT_SECRET)
     test_tracking = tracking_api.summary('13_digit_tracking_number')
+    test_pod = tracking_api.proof_of_delivery(TRACKING_NUMBER)
+    history_tracking = tracking_api.history(TRACKING_NUMBER)
+
+    print(test_tracking)
+    print(test_pod)
+    print(history_tracking)
+
+
