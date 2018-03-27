c1d4cb8961f5163a325d5843cd8d14e13cc59554
Alex Hellier <alex@absolutemusic.co.uk>
2018-03-27 11:23:32 +0100

    set back to standard, work on it later

--- tests/test_royal_mail_rest_api.py
+++ tests/test_royal_mail_rest_api.py
@@ -7,23 +7,19 @@ import pytest
 
 from click.testing import CliRunner
 
-from royal_mail_rest_api import *
+from royal_mail_rest_api import royal_mail_rest_api
 from royal_mail_rest_api import cli
-from royal_mail_rest_api.errors import *
 
 
 @pytest.fixture
-def http_response(status_code):
+def response():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-    class response_mock():
-        status_code = status_code
 
-    return response_mock()
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
@@ -40,23 +36,3 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
-
-
-def test_500_error(http_resonse):
-    """
-    Test a response.status_code 500 rasies GeneralError
-    :param http_resonse:
-    :return:
-    """
-    with pytest.raises(NotAuthorised):
-        RoyalMailBaseClass._test_error(http_response(500))
-
-def test_401_error(http_response):
-    """
-    test 401 raises not authed
-    :param http_response:
-    :return:
-    """
-    with pytest.raises(NotAuthorised):
-        RoyalMailBaseClass._test_error(http_response(401))
-
